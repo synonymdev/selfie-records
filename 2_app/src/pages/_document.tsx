1f3e20e946dc6d3afdb6eb2550e2836947951b56
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2024-09-10 15:45:24 -0300

    feat: Add analytics script to _document.tsx

--- 2_app/src/pages/_document.tsx
+++ 2_app/src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          data-domain="selfie-records.com"
+          src="https://_analytics.synonym.to/js/script.tagged-events.js"
+        ></script>
+      </Head>
       <body className="min-w-[400px] w-full">
         <Main />
         <NextScript />
